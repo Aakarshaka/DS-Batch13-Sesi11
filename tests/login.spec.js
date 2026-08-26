20c8676630a4fc3c1e1dc24f67527abe46f66c96
aakarshaka <aryasabdaps@gmail.com>
2026-08-26 18:11:44 +0700

    Update Login Test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,24 +1,42 @@
 import {Builder, By, until} from "selenium-webdriver";
-import { loginPage } from "../pages/loginPage.js";
+import { loginPage } from "../pages/LoginPage.js";
 import { expect } from "chai";
+import { captureScreenshot, compareScreenshots } from "../utils/VisualRegressionHelper.js";
 
 describe("Login Page", function () {
     let driver;
     let login;
 
-    beforeeach(async function () {
+    beforeEach(async function () {
         driver = await new Builder().forBrowser("chrome").build();
         login = new loginPage(driver);
         await login.open();
     });
 
-    aftereach(async function () {
+    afterEach(async function () {
         await driver.quit();
+    });
+
+    it("visual regression test for login page", async function () {
+        this.timeout(20000);
+        await driver.sleep(2000);
+        await captureScreenshot(driver, "./screenshots/actual/login.png");
+        const diffPixels = compareScreenshots(
+            "./screenshots/baseline/login.png",
+            "./screenshots/actual/login.png",
+            "./screenshots/diff/login_diff.png");
+        expect(diffPixels).to.equal(0);
     });
 
     it("should validate login with valid credentials", async function () {
         await login.login("standard_user", "secret_sauce");
         const currentUrl = await driver.getCurrentUrl();
-        expect(currentUrl).to.equal("https://saucedemo.com/inventory.html");
+        expect(currentUrl).to.equal("https://www.saucedemo.com/inventory.html");
+    });
+
+    it("get error message for invalid credentials", async function () {
+        await login.login("invalid_user", "invalid_password");
+        const errorMessage = await login.getErrorMessage();
+        expect(errorMessage).to.equal("Epic sadface: Username and password do not match any user in this service");
     });
 });
